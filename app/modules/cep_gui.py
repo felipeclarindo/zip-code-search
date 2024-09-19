5697bd5a47b6859d2f2f135e9bdfea2a52ea2cf9
felipeclarindo <fglpc2@gmail.com>
2024-09-19 09:47:48 -0300

    Configurando widgets da interface

--- app/modules/cep_gui.py
+++ app/modules/cep_gui.py
@@ -1,39 +1,79 @@
-from customtkinter import CTk, CTkLabel, CTkButton
+import customtkinter as ctk
 
+class CepGUI(ctk.CTk):
+    """
+    Classe principal da aplicação para busca de CEP.
+    Herda a classe CTk para criar uma interface gráfica personalizada.
+    """
+    
+    def __init__(self, informacoes_cep: dict) -> None:
+        """
+        Inicializa a interface gráfica, configura e define widgets da aplicação.
 
-class CepGUI(CTk):
-    def __init__(self, informacoes_cep) -> None:
-        super(CepGUI, self).__init__()
+        Args:
+            informacoes_cep (dict): Um dicionário contendo informações sobre o CEP.
+                Espera-se que as chaves e valores incluam:
+                - 'cep': O CEP associado.
+                - 'logradouro': O logradouro associado ao CEP.
+                - 'bairro': O bairro associado ao CEP.
+                - 'cidade': A cidade correspondente ao CEP.
+                - 'estado': O estado correspondente ao CEP.
+                - 'ddd': O DDD associado ao número.
+
+        Raises:
+            ValueError: Se as informações do CEP não estiverem completas.
+        """
+        super().__init__()
         self.informacoes_cep = informacoes_cep
         self.configure_screen()
         self.create_widgets()
 
     def configure_screen(self) -> None:
+        """
+        Configura a tela da aplicação, definindo título, tamanho, 
+        redimensionamento e tema de cores.
+        """
+        self.title("Informações do Cep")
         self.geometry("300x300")
         self.resizable(False, False)
-        self._set_appearance_mode("dark")
-
+        ctk.set_appearance_mode("dark")  # Definindo o modo escuro
+        ctk.set_default_color_theme("dark-blue")  # Definindo o tema de cores
+        
     def create_widgets(self) -> None:
-        self.label_title = CTkLabel(self, width=200, height=30, text="Informações")
-        self.label_title.pack()
+        """
+        Cria e organiza os widgets da interface gráfica,
+        incluindo labels e um botão de fechar.
+        """
+        # Usando a cor de fundo da janela para os labels
+        self.bg_color = self.cget("bg")
 
-        self.text_error = CTkLabel(self, text="")
-        self.text_error.pack()
+        title_label = ctk.CTkLabel(self, text="Informações do Cep", font=("Arial", 17), bg_color=self.cget("bg"))
+        title_label.pack(pady=15)
 
-        self.text_cep = CTkLabel(self, text="")
-        self.text_cep.pack()
+        cep_label = ctk.CTkLabel(self, text=f"CEP: {self.informacoes_cep.get('cep')}", bg_color=self.cget("bg"))
+        cep_label.pack()
 
-        self.text_rua = CTkLabel(self, text="")
-        self.text_rua.pack()
+        rua_label = ctk.CTkLabel(self, text=f"Rua: {self.informacoes_cep.get('rua')}", bg_color=self.cget("bg"))
+        rua_label.pack()
 
-        self.text_bairro = CTkLabel(self, text="")
-        self.text_bairro.pack()
+        bairro_label = ctk.CTkLabel(self, text=f"Bairro: {self.informacoes_cep.get('bairro')}", bg_color=self.cget("bg"))
+        bairro_label.pack()
 
-        self.text_cidade = CTkLabel(self, text="")
-        self.text_cidade.pack()
+        cidade_label = ctk.CTkLabel(self, text=f"Cidade: {self.informacoes_cep.get('cidade')}", bg_color=self.cget("bg"))
+        cidade_label.pack()
 
-        self.text_ddd = CTkLabel(self, text="")
-        self.text_ddd.pack()
+        ddd_label = ctk.CTkLabel(self, text=f"DDD: {self.informacoes_cep.get('ddd')}", bg_color=self.cget("bg"))
+        ddd_label.pack()
 
-        self.button_close = CTkButton(self, width=80, height=40, text="Close")
-        self.button_close.pack()
+        close_button = ctk.CTkButton(self, text="Fechar", command=self.destroy)
+        close_button.pack(pady=20)
+
+if __name__ == "__main__":
+    app = CepGUI({
+        "cep": "124",
+        "rua": "Rua 3",
+        "bairro": "Bela Vista",
+        "cidade": "São Paulo",
+        "ddd": "011"
+    })
+    app.mainloop()
